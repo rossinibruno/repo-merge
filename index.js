5e4e6c6ddf9c76f94e5a72fe9833a82e0c8bb607
Bruno Rossini <bruno.rossini@banqi.com>
2022-05-17 09:19:55 -0300

    implementa ... na mensagem de log

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function oddOrEven(x) {
+<<<<<<< HEAD
   console.log('Log de início de função!')
+=======
+  console.log('Log de início de função...')
+>>>>>>> implementa ... na mensagem de log
   if (x % 2 == 0) return console.log('O Número é Par')
 
   return console.log('O Número é Ímpar')
